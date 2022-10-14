86368ac1bacfe3e2fb46dff62a5511425d70792b
Diogo Nunes <diogo.nunes>
2022-10-14 09:43:44 -0300

    list

--- dist/ftp.d.ts
+++ dist/ftp.d.ts
@@ -1,5 +1,5 @@
-import SFTPClient from "ssh2-sftp-client";
 import { ConfigAuthFTP, FTPInterface } from "sdz-agent-types";
+import SFTPClient, { FileInfo } from "ssh2-sftp-client";
 declare class FTP implements FTPInterface {
     private config;
     constructor(config: ConfigAuthFTP);
@@ -8,5 +8,6 @@ declare class FTP implements FTPInterface {
     sendFile(localFileName: string, remoteFileName: string): Promise<boolean>;
     getFile(remoteFileName: string, localFileName: string): Promise<boolean>;
     renameFile(remoteFileName: string, newRemoteFileName: string): Promise<boolean>;
+    list(path: string): Promise<FileInfo[]>;
 }
 export default FTP;
